c47ee63020c6657fd301c0b9e77822fca11813f6
1liale <1alexli2002@gmail.com>
2023-07-04 22:00:41 -0400

    ci

--- backend/index.mjs
+++ backend/index.mjs
@@ -2,7 +2,7 @@ export const handler = async(event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Helloo 12!'),
+        body: JSON.stringify('Helloo again!'),
     };
     return response;
 };
