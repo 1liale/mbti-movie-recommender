07b1780f43703982e477bdca80e754bb96891b78
1liale <1alexli2002@gmail.com>
2023-07-04 22:41:31 -0400

    upgraded to node 14 runtime

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const faker = require('faker')
+
+exports.handler = async (event) => {
+  // TODO implement
+  const movie = {
+    title: faker.lorem.words(),
+    plot: faker.lorem.paragraph(),
+    director: `${faker.name.firstName()} ${faker.name.lastName()}`,
+    image: faker.image.abstract(),
+  };
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(movie),
+  };
+  return response;
+};
