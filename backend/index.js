c8f330ce6f7c917e041f7857e64c312936243852
1liale <1alexli2002@gmail.com>
2023-07-04 20:33:33 -0400

    updated CI and added mock data test on backend using faker

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,16 @@
+const faker = require("faker");
+
 exports.handler = async (event) => {
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify("Hello from Lambda and Github!"),
-    }
-    return response
-}
+  // TODO implement
+  const movie = {
+    title: faker.lorem.words(),
+    plot: faker.lorem.paragraph(),
+    director: `${faker.name.firstName()} ${faker.name.lastName()}`,
+    image: faker.image.abstract(),
+  };
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(movie),
+  };
+  return response;
+};
