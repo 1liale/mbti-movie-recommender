d07a02e2f5265f26857f12c998d37ca402dda50d
1liale <1alexli2002@gmail.com>
2023-09-02 23:02:12 -0400

    appsync api basic setup with mongodb connection

--- backend/lib/apiStack.ts
+++ backend/lib/apiStack.ts
@@ -1,40 +1,32 @@
-import * as cdk from 'aws-cdk-lib'
-import { Construct } from 'constructs'
-import * as path from 'path'
-import * as appsync from 'aws-cdk-lib/aws-appsync'
-import { PolicyStatement } from 'aws-cdk-lib/aws-iam'
-import { CfnFunctionConfiguration, CfnResolver } from 'aws-cdk-lib/aws-appsync'
-import { readFileSync } from 'fs'
+import { CfnOutput, Stack, StackProps } from 'aws-cdk-lib'
+import { AppsyncFunction, AuthorizationType, Code, FunctionRuntime, GraphqlApi, SchemaFile } from 'aws-cdk-lib/aws-appsync';
+import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { Construct } from 'constructs';
+import { join } from 'path';
 
-interface ApiStackProps extends cdk.StackProps {
-	MONGO_SECRET_ARN: string
+interface AppsyncMongoAPIStackProps extends StackProps {
+	MONGO_SECRET_ARN: string;
 }
 
-export class ApiStack extends cdk.Stack {
-	constructor(scope: Construct, id: string, props: ApiStackProps) {
-		super(scope, id, props)
+export class AppsyncMongoAPIStack extends Stack {
+	constructor(scope: Construct, id: string, props: AppsyncMongoAPIStackProps) {
+		super(scope, id, props);
 
-		//create our API
-		const api = new appsync.GraphqlApi(this, 'AppSyncToMongoAPI', {
-			name: 'AppSyncToMongoAPI',
-			schema: appsync.SchemaFile.fromAsset(
-				path.join(__dirname, 'graphql/schema.graphql')
+		// Create Appsync GraphQL Api
+		const api = new GraphqlApi(this, 'GraphqlApi', {
+			name: "GraphqlApi",
+			schema: SchemaFile.fromAsset(
+				join(__dirname, '/graphql/schema.graphql')
 			),
 			authorizationConfig: {
 				defaultAuthorization: {
-					authorizationType: appsync.AuthorizationType.API_KEY,
+					authorizationType: AuthorizationType.API_KEY,
 				},
 			},
-			logConfig: {
-				fieldLogLevel: appsync.FieldLogLevel.ALL,
-			},
-			xrayEnabled: true,
-		})
+		});
 
-		// Create our 2 datasources
-
-		// Secrets Manager
-		const secretsManagerDS = api.addHttpDataSource(
+		// Secrets Manager Datasource
+		const SMDatasrc = api.addHttpDataSource(
 			'secretsManager',
 			`https://secretsmanager.${process.env.CDK_DEFAULT_REGION}.amazonaws.com`,
 			{
@@ -43,116 +35,47 @@ export class ApiStack extends cdk.Stack {
 					signingServiceName: 'secretsmanager',
 				},
 			}
-		)
+		);
 
-		// The MongoDB API
-		const mongoDataAPIDS = api.addHttpDataSource(
-			'mongoDBAtlasCluster',
-			'https://data.mongodb-api.com'
-		)
-
-		// policy to permit an http datasource to get a secret in Secrets Manager
-		secretsManagerDS.grantPrincipal.addToPrincipalPolicy(
+		// Attach a policy to allow http datasource to fetch a secret from AWS SM
+		SMDatasrc.grantPrincipal.addToPrincipalPolicy(
 			new PolicyStatement({
 				resources: [props.MONGO_SECRET_ARN],
-				actions: ['secretsmanager:GetSecretValue'],
+				actions: ['secretsmanager:GetSecretValue']
 			})
-		)
-		///////////////
-		// Create a function that gets the secret
-		const getMongoSecretFunc = new appsync.AppsyncFunction(
+		);
+
+		// Function to get the secret
+		const getMongoSecretFunc = new AppsyncFunction(
 			this,
 			'getMongoSecretFunc',
 			{
 				api,
-				dataSource: secretsManagerDS,
-				name: 'getMongoSecretFromSSM',
-				code: appsync.Code.fromAsset(
-					path.join(__dirname, '/graphql/mappings/getMongoSecret.js')
+				dataSource: SMDatasrc,
+				name: 'getMongoSecretFromSM',
+				code: Code.fromAsset(
+					join(__dirname, '/graphql/mappings/getMongoSecret.js')
 				),
-				runtime: appsync.FunctionRuntime.JS_1_0_0,
+				runtime: FunctionRuntime.JS_1_0_0,
 			}
 		)
 
-		////////////////
+		// MongoDB API Datasource
+		const mongoDBApiDatasrc = api.addHttpDataSource(
+			'mongoDBAtlasCluster',
+			'https://data.mongodb-api.com'
+		);
 
-		// Create a function that will list the Applicants from MongoDB
-		const listApplicantsFunction = new appsync.AppsyncFunction(
-			this,
-			'listApplicantsFunction',
-			{
-				api,
-				dataSource: mongoDataAPIDS,
-				name: 'listApplicantsFunction',
-				code: appsync.Code.fromAsset(
-					path.join(__dirname, '/graphql/mappings/listApplicants.js')
-				),
-				runtime: appsync.FunctionRuntime.JS_1_0_0,
-			}
-		)
-
-		////////////////////
-		// Create a function that will add an Applicant to MongoDB
-		const addApplicantFunction = new appsync.AppsyncFunction(
-			this,
-			'addApplicantFunction',
-			{
-				api,
-				dataSource: mongoDataAPIDS,
-				name: 'addApplicantFunction',
-				code: appsync.Code.fromAsset(
-					path.join(__dirname, '/graphql/mappings/addApplicant.js')
-				),
-				runtime: appsync.FunctionRuntime.JS_1_0_0,
-			}
-		)
-		///////////////
-
-		// Create a pipeline that has a "before" and "after" step + our fns
-		const listApplicantsPipelineResolver = new appsync.Resolver(
-			this,
-			'listApplicantsPipelineResolver',
-			{
-				api,
-				typeName: 'Query',
-				fieldName: 'listApplicants',
-				runtime: appsync.FunctionRuntime.JS_1_0_0,
-				code: appsync.Code.fromAsset(
-					path.join(__dirname, '/graphql/mappings/pipeline.js')
-				),
-				pipelineConfig: [getMongoSecretFunc, listApplicantsFunction],
-			}
-		)
-
-		///////////////////
-
-		// Create a pipeline that has a "before" and "after" step + our fns
-		const addApplicantPipelineResolver = new appsync.Resolver(
-			this,
-			'addApplicantPipelineResolver',
-			{
-				api,
-				typeName: 'Mutation',
-				fieldName: 'addApplicant',
-				runtime: appsync.FunctionRuntime.JS_1_0_0,
-				code: appsync.Code.fromAsset(
-					path.join(__dirname, '/graphql/mappings/pipeline.js')
-				),
-				pipelineConfig: [getMongoSecretFunc, addApplicantFunction],
-			}
-		)
-
-		//////////////////////////
-		new cdk.CfnOutput(this, 'appsync api key', {
+		new CfnOutput(this, 'appsync api key', {
 			value: api.apiKey!,
-		})
+		});
 
-		new cdk.CfnOutput(this, 'appsync endpoint', {
+		new CfnOutput(this, 'appsync endpoint', {
 			value: api.graphqlUrl,
-		})
+		});
 
-		new cdk.CfnOutput(this, 'appsync apiId', {
+		new CfnOutput(this, 'appsync apiId', {
 			value: api.apiId,
-		})
+		});
 	}
 }
