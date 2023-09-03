ff37cb12018a0188c2762691497b5b056d1f4b41
1liale <1alexli2002@gmail.com>
2023-09-02 23:50:56 -0400

    added svelte-flowbite and tailwindcss

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -5,13 +5,13 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: [vitePreprocess({})],
 
 	kit: {
 		adapter: adapter({
-		  fallback: 'index.html'
+			fallback: 'index.html'
 		})
-	  }
+	}
 };
 
 export default config;
